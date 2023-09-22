feb5fee32adf97d36ce7c8a4c794bb621698a589
Pingvu <nhlpool27@gmail.com>
2023-09-22 14:43:05 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilken är din favoritbok?");
+            string favoritbok = Console.ReadLine();
+            Console.WriteLine(favoritbok + " Är en bra bok");
+            Console.ReadKey();
+        }
+    }
+}
